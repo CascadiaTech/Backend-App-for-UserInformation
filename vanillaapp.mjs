0a1082eaab51e332e0374fecc6d890b08470b1cd
unknown <hollowaycryptoconsulting@gmail.com>
2022-09-15 12:10:29 -0700

    newapptsting

--- vanillaapp.mjs
+++ vanillaapp.mjs
@@ -9,7 +9,7 @@ app.use(cors())
 
 app.use(express.static("public"))
 
-app.get('/',async (req, res) => {
+app.get('/',(req, res) => {
 
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
@@ -17,8 +17,8 @@ app.get('/',async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-
-if (req.method == "GET") {
+    async function Fetchaccnts() {
+      if (req.method == "GET")
       try {
         const uri = 'mongodb+srv://Flaskapp:iamdiablo@clustertest.rxvyc0x.mongodb.net/?retryWrites=true&w=majority'
         const client = new MongoClient(uri)
@@ -29,43 +29,32 @@ if (req.method == "GET") {
         const allValues = await cursor.toArray()
         //console.log(allValues)
         //const final = await cursor.forEach((doc) => console.log(doc))
-        const final = JSON.stringify(allValues)
-        res.send(final)
-        return final
+        return allValues
       } catch (error) {
         console.log(error)
       } finally {
         const uri = 'mongodb+srv://Flaskapp:iamdiablo@clustertest.rxvyc0x.mongodb.net/?retryWrites=true&w=majority'
         const client = new MongoClient(uri)
         await client.close()
-}}     
+      }
+    }
 
-if (req.method == "POST") {
-    try {
-
-        const uri = 'mongodb+srv://Flaskapp:iamdiablo@clustertest.rxvyc0x.mongodb.net/?retryWrites=true&w=majority'
-        const client = new MongoClient(uri)
-        await client.connect()
-        const database = client.db('Userinfo')
-        const movies = database.collection('Userinformation')
-
-        const doc = {
-            address: req.body.address,
-            name: req.body.name,
-          }
-          const result = await movies.insertOne(doc);
-          return result
-    } catch(error) {
-    console.log(error)
-    }finally {
-    const uri = 'mongodb+srv://Flaskapp:iamdiablo@clustertest.rxvyc0x.mongodb.net/?retryWrites=true&w=majority'
-    const client = new MongoClient(uri)
-    await client.close()
-  }
+  Fetchaccnts()
+  .then((result) => (JSON.stringify(result)))
+  .then((result) => (res.send(result)))
   
-}})
+})
+
+// start the server listening for requests
 app.listen(process.env.PORT || 5500, 
 	() => console.log("Server is running..."));
 
 
 
+
+//http://127.0.0.1:5500/
+
+//const port = 5500
+//const host = '127.0.0.1'
+//server.listen(port, host)
+//console.log(`Listening at http://${host}:${port}`)
